d25df4f89c7c1856b1bc0e7a89dbd534021b42fb
Librecz Bernadett <librecz.bernadett@gmail.com>
2015-03-30 19:49:53 +0200

    add first row

--- xml_excel.rb
+++ xml_excel.rb
@@ -2,7 +2,16 @@ File.open('excel.xls', 'w') do |file|
 	file.write('<?xml version="1.0"?>
 		<ss:Workbook xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">
 		<ss:Worksheet ss:Name="First sheet">
-        
+        <ss:Table>
+            <ss:Row>
+                <ss:Cell>
+                    <ss:Data ss:Type="String">Header1</ss:Data>
+                </ss:Cell>
+                <ss:Cell>
+                    <ss:Data ss:Type="String">Header2</ss:Data>
+                </ss:Cell>
+            </ss:Row>
+        </ss:Table>
     </ss:Worksheet>   
 		</ss:Workbook>')
 end
