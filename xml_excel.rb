3f3d8bbf929a8308ca7c57703c52e7c18388d32e
Librecz Bernadett <librecz.bernadett@gmail.com>
2015-03-30 19:51:10 +0200

    make first row bold

--- xml_excel.rb
+++ xml_excel.rb
@@ -1,9 +1,14 @@
 File.open('excel.xls', 'w') do |file|
 	file.write('<?xml version="1.0"?>
-		<ss:Workbook xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">
+		<ss:Workbook xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">		
+    <ss:Styles>
+        <ss:Style ss:ID="bold">
+            <ss:Font ss:Bold="1"/>
+        </ss:Style>
+    </ss:Styles>
 		<ss:Worksheet ss:Name="First sheet">
         <ss:Table>
-            <ss:Row>
+            <ss:Row ss:StyleID="bold">
                 <ss:Cell>
                     <ss:Data ss:Type="String">Header1</ss:Data>
                 </ss:Cell>
