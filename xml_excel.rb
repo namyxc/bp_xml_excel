ebfeae313875893972b67381cc9fdcdf2b75a788
Librecz Bernadett <librecz.bernadett@gmail.com>
2015-03-30 19:48:20 +0200

    add named sheet

--- xml_excel.rb
+++ xml_excel.rb
@@ -1,5 +1,8 @@
 File.open('excel.xls', 'w') do |file|
 	file.write('<?xml version="1.0"?>
-		<ss:Workbook xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">    
+		<ss:Workbook xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">
+		<ss:Worksheet ss:Name="First sheet">
+        
+    </ss:Worksheet>   
 		</ss:Workbook>')
 end
